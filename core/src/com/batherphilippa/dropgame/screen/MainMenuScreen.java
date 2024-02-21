e0290430da2692f3b6f82f6088a799d84b5f63d5
pabather <p.a.bather1@gmail.com>
2024-02-21 09:03:24 +0100

    added exit option

--- core/src/com/batherphilippa/dropgame/screen/MainMenuScreen.java
+++ core/src/com/batherphilippa/dropgame/screen/MainMenuScreen.java
@@ -1,6 +1,7 @@
 package com.batherphilippa.dropgame.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -32,11 +33,17 @@ public class MainMenuScreen implements Screen {
         game.batch.begin();
         game.font.draw(game.batch, "Welcome to Drop!!!", 250, 250); // x and y: position of font on screen
         game.font.draw(game.batch, "Tap anywhere to begin!", 250, 200);
+        game.font.draw(game.batch, "Press ESCAPE to exit", 250, 150);
         game.batch.end();
 
         if (Gdx.input.isTouched()) {
             game.setScreen(new GameScreen(game));
             dispose(); // dispose of MainMenuScreen
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            dispose();
+            System.exit(0);  // zero indicates non abnormal termination
         }
     }
 
