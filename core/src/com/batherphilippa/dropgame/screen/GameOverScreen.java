d21a03bdcfc206920706788feb85623d88c14e5b
pabather <p.a.bather1@gmail.com>
2024-02-25 10:55:39 +0100

    refactored

--- core/src/com/batherphilippa/dropgame/screen/GameOverScreen.java
+++ core/src/com/batherphilippa/dropgame/screen/GameOverScreen.java
@@ -1,2 +1,71 @@
-package com.batherphilippa.dropgame.screen;public class GameOverScreen {
+package com.batherphilippa.dropgame.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.batherphilippa.dropgame.Drop;
+import com.batherphilippa.dropgame.manager.CameraManager;
+
+public class GameOverScreen implements Screen {
+
+    private final Drop game;
+    private CameraManager cameraManager;
+    private int points;
+
+    public GameOverScreen(Drop game, int points) {
+        this.game = game;
+        this.cameraManager = new CameraManager();
+        this.points = points;
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0.5f, 0, 0.2f, 5);
+
+        cameraManager.update();
+        cameraManager.setProjectionMatrix(game);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Game Over!!!", 250, 250);
+        game.font.draw(game.batch, "Points collected: " + points, 250, 200);
+        game.font.draw(game.batch, "Press Escape to Exit", 250, 150);
+        game.batch.end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            dispose();
+            System.exit(0);
+        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
--- core/src/com/batherphilippa/dropgame/screen/MainMenuScreen.java
+++ core/src/com/batherphilippa/dropgame/screen/MainMenuScreen.java
@@ -3,22 +3,18 @@ package com.batherphilippa.dropgame.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.batherphilippa.dropgame.Drop;
-
-import static com.batherphilippa.dropgame.utils.ScreenConstants.VIEWPORT_HEIGHT;
-import static com.batherphilippa.dropgame.utils.ScreenConstants.VIEWPORT_WIDTH;
+import com.batherphilippa.dropgame.manager.CameraManager;
 
 public class MainMenuScreen implements Screen {
 
     private final Drop game;
-    private OrthographicCamera camera;
+    private CameraManager cameraManager;
 
     public MainMenuScreen(Drop game) {
         this.game = game;
-        this.camera = new OrthographicCamera();
-        this.camera.setToOrtho(false, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+        this.cameraManager = new CameraManager();
     }
 
     @Override
@@ -30,8 +26,8 @@ public class MainMenuScreen implements Screen {
     public void render(float delta) {
         ScreenUtils.clear(0.5f, 0, 0.2f, 5);
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
+        cameraManager.update();
+        cameraManager.setProjectionMatrix(game);
 
         game.batch.begin();
         game.font.draw(game.batch, "Welcome to Drop!!!", 250, 250); // x and y: position of font on screen
