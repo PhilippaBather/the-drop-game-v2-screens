6ba39653b28a91e711e4f6176b414c5f36004ae8
pabather <p.a.bather1@gmail.com>
2024-02-25 21:39:46 +0100

    updated

--- core/src/com/batherphilippa/dropgame/utils/ConfigConstants.java
+++ core/src/com/batherphilippa/dropgame/utils/ConfigConstants.java
@@ -2,5 +2,12 @@ package com.batherphilippa.dropgame.utils;
 
 public class ConfigConstants {
 
-    public static final float GAME_TIME = 30; // seconds
+    public static final float GAME_LENGTH_30S = 30; // seconds
+    public static final float GAME_LENGTH_45S = 45; // seconds
+    public static final float GAME_LENGTH_60S = 60; // seconds
+
+    public static final String GAME_LENGTH_STR_30S = "30 seconds";
+    public static final String GAME_LENGTH_STR_45S = "45 seconds";
+    public static final String GAME_LENGTH_STR_60S = "1 minute";
+    public static final String APP_NAME = "TheDropGAme";
 }
--- core/src/com/batherphilippa/dropgame/utils/UIConstants.java
+++ core/src/com/batherphilippa/dropgame/utils/UIConstants.java
@@ -1,2 +1,14 @@
-package com.batherphilippa.dropgame.utils;public class UIConstants {
+package com.batherphilippa.dropgame.utils;
+
+public class UIConstants {
+
+    public static final String BTN_CONFIG = "CONFIGURATIONS";
+    public static final String BTN_EXIT = "EXIT";
+    public static final String BTN_PLAY = "PLAY";
+    public static final String BTN_RETURN = "RETURN";
+
+    public static final String LABEL_DARK_MODE = "Dark Mode";
+    public static final String LABEL_GAME_LENGTH = "Select Game Length";
+    public static final String LABEL_OPTIONS_MENU_TITLE = "SELECT CONFIGURATION OPTIONS";
+    public static final String LABEL_WELCOME = "Welcome to Drop!!!";
 }
