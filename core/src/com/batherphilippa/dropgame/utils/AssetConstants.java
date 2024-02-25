995b2bfea45e76ad0c047586dd05a9858ddcec55
pabather <p.a.bather1@gmail.com>
2024-02-25 15:08:52 +0100

    updated

--- core/src/com/batherphilippa/dropgame/utils/AssetConstants.java
+++ core/src/com/batherphilippa/dropgame/utils/AssetConstants.java
@@ -3,7 +3,9 @@ package com.batherphilippa.dropgame.utils;
 public class AssetConstants {
 
     public static final String CHARACTER_PLAYER = "bucket";
-    public static final String CHARACTER_ITEM = "drop";
+    public static final String CHARACTER_ITEM_DROP = "drop";
+    public static final String CHARACTER_ITEM_ENEMY = "stone";
     public static final String SOUND_DROP = "raindrop";
+    public static final String SOUND_STONE = "stone";
     public static final String MUSIC_THEME = "theme_music";
 }
