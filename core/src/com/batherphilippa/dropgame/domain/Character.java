1370e0857e59a2e84830ac76e8e47eaf05c0230f
pabather <p.a.bather1@gmail.com>
2024-02-26 09:54:25 +0100

    refactored for atlas; redundant code removed; comments

--- core/src/com/batherphilippa/dropgame/domain/Character.java
+++ core/src/com/batherphilippa/dropgame/domain/Character.java
@@ -1,26 +1,28 @@
 package com.batherphilippa.dropgame.domain;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.batherphilippa.dropgame.utils.KeyDirection;
 
 public abstract class Character {
 
-    Texture texture;
+//    Texture texture;
+    TextureRegion region;
     Rectangle rectangle;
     Vector2 coords;
 
-    public Character(Texture texture) {
-        this.texture = texture;
+    public Character(TextureRegion region) {
+        this.region = region;  // TextureRegion instead of Texture, as using TextureAtlas
         this.rectangle = new Rectangle();
         this.coords = new Vector2();
     }
 
     public abstract void setInitialCoordinates();
     public void render(SpriteBatch batch) {
-        batch.draw(texture, rectangle.x, rectangle.y);
+//        batch.draw(texture, rectangle.x, rectangle.y);  // previous code
+        batch.draw(region, rectangle.x, rectangle.y);   // using TextureAtlas
     }
     public abstract void move(Vector2 position, KeyDirection dir);
     public abstract void update();
