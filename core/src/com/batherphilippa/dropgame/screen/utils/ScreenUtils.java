e12fab4790df0dd8d21994adac89277c116349c6
pabather <p.a.bather1@gmail.com>
2024-03-05 11:03:23 +0100

    added clear method

--- core/src/com/batherphilippa/dropgame/screen/utils/ScreenUtils.java
+++ core/src/com/batherphilippa/dropgame/screen/utils/ScreenUtils.java
@@ -1,2 +1,12 @@
-package com.batherphilippa.dropgame.screen.utils;public class ScreenUtils {
+package com.batherphilippa.dropgame.screen.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class ScreenUtils {
+
+    public static void clearScreen(float red, float green, float blue, float alpha) {
+        com.badlogic.gdx.utils.ScreenUtils.clear(red, green, blue, alpha);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
 }
