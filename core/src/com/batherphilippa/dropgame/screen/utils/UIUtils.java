6b12fc94268e96f54796bb78c254d4113b776d32
pabather <p.a.bather1@gmail.com>
2024-03-06 11:21:04 +0100

    added method to set points

--- core/src/com/batherphilippa/dropgame/screen/utils/UIUtils.java
+++ core/src/com/batherphilippa/dropgame/screen/utils/UIUtils.java
@@ -2,11 +2,18 @@ package com.batherphilippa.dropgame.screen.utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.kotcrab.vis.ui.widget.VisTable;
 
 public class ScreenUtils {
 
     public static void clearScreen(float red, float green, float blue, float alpha) {
         com.badlogic.gdx.utils.ScreenUtils.clear(red, green, blue, alpha);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    public static VisTable createTableObj() {
+        VisTable table = new VisTable(true);
+        table.setFillParent(true);
+        return table;
     }
 }
